1e97b022938bb0365947d7fe010ab88b8cab7fe2
zsr12383 <zsr123@naver.com>
2022-12-26 20:44:25 +0900

    feat: 백그라운드 이벤트 로직 작성

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,66 @@
-function polling() {
-  // console.log("polling");
-  setTimeout(polling, 1000 * 30);
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ enabled: true });
+  console.log("Default option set true");
+});
+
+chrome.runtime.onMessage.addListener((message /*sender, sendResponse*/) => {
+  if (typeof message === "object" && message.type === "btnChange") {
+    console.log("btnChange");
+    runBtnChangeCallBack(chrome);
+  }
+  return true;
+});
+
+chrome.webNavigation.onCompleted.addListener(function (e) {
+  console.log("onCompleted");
+  chrome.storage.local.get("enabled", (data) => {
+    if (isValidTrigger(data, e)) runScript();
+  });
+});
+
+chrome.webNavigation.onHistoryStateUpdated.addListener(function (e) {
+  console.log("onHistoryStateUpdated");
+  chrome.storage.local.get("enabled", (data) => {
+    if (isValidTrigger(data, e)) runScript();
+  });
+});
+
+async function runBtnChangeCallBack(chrome: typeof globalThis.chrome) {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  if (
+    !tab ||
+    !tab.url ||
+    !tab.url.match(/^https:\/\/www.youtube.com\/watch?.+/)
+  ) {
+    console.log(tab);
+    console.log("Invalid_URL");
+    return;
+  }
+  chrome.storage.local.get("enabled", (data) => {
+    if (data.enabled) {
+      chrome.scripting.executeScript({
+        // @ts-ignore
+        target: { tabId: tab.id },
+        files: ["content.js"],
+      });
+      console.log("success");
+    } else {
+      // 여기서 컨텐츠로 메시지 보내거나 아니면 그냥 바로 팝업에서 보내도 될듯?
+      console.log("button off");
+    }
+  });
 }
 
-polling();
+async function runScript() {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  if (!tab.id) return;
+  await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ["content.js"],
+  });
+}
+
+const isValidTrigger = (
+  data: { [x: string]: any; enabled?: any },
+  e: chrome.webNavigation.WebNavigationFramedCallbackDetails
+) => data.enabled && e.url.match(/^https:\/\/www.youtube.com\/watch?.+/);
