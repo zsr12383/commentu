eaac6a99877b36ad5c69cfe6ea37c0c731793a25
zsr12383 <zsr123@naver.com>
2023-01-03 18:42:20 +0900

    fix: lint error 수정

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,5 +1,21 @@
-const ReplyExtractor = class {
-  static instance: never;
+function getTime(currentTimeString: string) {
+  if (currentTimeString.length > 5)
+    return (
+      parseInt(currentTimeString.replace(/:.+/, ''), 10) * 3600 +
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      parseInt(currentTimeString.match(/:([0-9]+):/)[1], 10) * 60 +
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      parseInt(currentTimeString.match(/[0-9]+$/)[0], 10)
+    );
+  return (
+    parseInt(currentTimeString.replace(/:[0-9]+/, ''), 10) * 60 + parseInt(currentTimeString.replace(/[0-9]+:/, ''), 10)
+  );
+}
+
+class ReplyExtractor {
+  static instance: ReplyExtractor;
 
   private readonly comment: Array<any>;
 
@@ -9,7 +25,7 @@ const ReplyExtractor = class {
 
   private url: string;
 
-  private inter: never | undefined;
+  private inter: NodeJS.Timer | undefined;
 
   private rand: number;
 
@@ -21,11 +37,9 @@ const ReplyExtractor = class {
 
   private title: never | undefined;
 
-  private video: Element | undefined;
+  private video: HTMLVideoElement | undefined;
 
-  private btn: undefined;
-
-  private b: object | undefined;
+  private b: { [index: number]: string };
 
   constructor() {
     this.comment = [];
@@ -35,118 +49,95 @@ const ReplyExtractor = class {
     this.target = window.location.search.match(/=([^=&/]+)/)[1];
     this.url = `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&key=${this.api_key}&videoId=${this.target}&maxResults=100`;
     this.rand = Math.random();
+    this.b = {};
 
-    (async () => {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      await fetch(this.url)
-        .then((res) => res.json())
-        .then((data) => {
-          data.items.forEach((ele: { snippet: { topLevelComment: { snippet: { textDisplay: never } } } }) => {
-            this.comment.push(ele.snippet.topLevelComment.snippet.textDisplay);
-          });
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    fetch(this.url)
+      .then((res) => res.json())
+      .then((data) => {
+        data.items.forEach((ele: { snippet: { topLevelComment: { snippet: { textDisplay: never } } } }) => {
+          this.comment.push(ele.snippet.topLevelComment.snippet.textDisplay);
         });
-      this.runtime = document.getElementsByClassName('ytp-time-current')[0];
+        this.runtime = document.getElementsByClassName('ytp-time-current')[0];
 
-      this.h1 = undefined;
-      while (!this.h1) this.h1 = document.getElementsByTagName('h1');
+        // this.h1 = undefined;
+        // while (!this.h1) this.h1 = document.getElementsByTagName('h1');
 
-      this.title = this.h1[1].innerText;
-      this.video = document.getElementsByClassName('video-stream html5-main-video')[0];
-      this.btn = undefined;
-      this.b = {};
-      let tmp = null;
+        this.h1 = document.getElementsByTagName('h1');
+        this.title = this.h1[1].innerText;
+        this.video = document.getElementsByClassName('video-stream html5-main-video')[0] as HTMLVideoElement;
 
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      this.comment.forEach((ele) => {
-        // eslint-disable-next-line no-cond-assign
-        if ((tmp = ele.match(/<a href[^<>]+>(([0-9]+:)?[0-9]+:[0-9]+)</))) {
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
-          this.b[this.eval_time(tmp[1])] = i;
-        }
+        let tmp = null;
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        this.comment.forEach((ele) => {
+          // eslint-disable-next-line no-cond-assign
+          if ((tmp = ele.match(/<a href[^<>]+>(([0-9]+:)?[0-9]+:[0-9]+)</))) {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            this.b[getTime(tmp[1])] = ele;
+          }
+        });
       });
-
-      this.on_active(this);
-    })();
-  }
-
-  eval_time(str) {
-    if (str.length > 5)
-      return (
-        parseInt(str.replace(/:.+/, '')) * 3600 +
-        parseInt(str.match(/:([0-9]+):/)[1]) * 60 +
-        parseInt(str.match(/[0-9]+$/)[0])
-      );
-    return parseInt(str.replace(/:[0-9]+/, '')) * 60 + parseInt(str.replace(/[0-9]+:/, ''));
   }
 
   off_active() {
-    // popup.js나 background에서 메시지 콜백 식으로 가자..
-    console.log(this);
-    clearInterval(this.inter);
-    this.h1[1].innerText = this.title;
+    if (this.inter) clearInterval(this.inter);
+    if (this.h1) this.h1[1].innerText = this.title;
   }
 
-  on_active(self) {
-    (() => {
-      let run;
-      self.inter = setInterval(function () {
-        run = Math.floor(self.video.currentTime); // string으로 해야되나? 확인한번
-        console.log(run);
-        if (/* self.btn && */ self.b[run]) {
-          self.h1[1].innerText = self.b[run]; // 나중에 자막으로 삽입
-          console.log(self.h1[1].innerText);
-        }
-      }, 1000);
-
-      self.video.onplay = () => {
-        let run;
-        clearInterval(self.inter);
-        self.inter = setInterval(function () {
-          run = Math.floor(self.video.currentTime);
-          console.log(run);
-          if (/* self.btn && */ self.b[run]) {
-            self.h1[1].innerText = self.b[run];
-            console.log(self.h1[1].innerText);
-          }
-        }, 1000);
-      };
-
-      self.video.onpause = function () {
-        clearInterval(self.inter);
-      };
-
-      window.onunload = function () {
-        clearInterval(self.inter);
-        self = null;
-      };
-
-      window.onpopstate = () => {
-        clearInterval(self.inter);
-      }; // <-- 새로 클래스 만들어서 적용될 수 있도록 구현
-    })();
+  setIntervalText(): undefined | NodeJS.Timer {
+    let run;
+    if (this.video === undefined) return undefined;
+    return setInterval(() => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      run = Math.floor(this.video.currentTime); // string으로 해야되나? 확인한번
+      if (this.b && this.b[run] && this.h1) {
+        this.h1[1].innerText = this.b[run]; // 나중에 자막으로 삽입
+      }
+    }, 1000);
   }
-};
+
+  on_active() {
+    if (this.video === undefined) return;
+
+    this.inter = this.setIntervalText();
+
+    this.video.onplay = () => {
+      if (this.inter) clearInterval(this.inter);
+      this.inter = this.setIntervalText();
+    };
+
+    this.video.onpause = () => {
+      if (this.inter) clearInterval(this.inter);
+    };
+
+    window.onunload = () => {
+      if (this.inter) clearInterval(this.inter);
+    };
+
+    window.onpopstate = () => {
+      if (this.inter) clearInterval(this.inter);
+    };
+  }
+}
 
 function getCurrentDom() {
   let currentDom;
   if (document.body) currentDom = document.body;
-  else if (document.documentElement) currentDom = documentElement;
-  else console.log('Invalid Page');
+  else if (document.documentElement) currentDom = document.documentElement;
   return currentDom;
 }
-
-// 익명함수 처리 필요할듯?
 
 const currentDom = getCurrentDom();
 if (currentDom) {
   currentDom.addEventListener(
     'transitionend',
-    function (/* TransitionEvent */ event) {
-      if (event.propertyName === 'width' && event.target.id === 'progress') {
-      }
+    (/* TransitionEvent */ event) => {
+      if (event.propertyName === 'width' && event.target /* && event.target.id && event.target.id === 'progress' */)
+        console.log('test');
     },
     true,
   );
