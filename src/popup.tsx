ef23b16852a9cafdc453415c446d3ce15fc0c261
zsr12383 <zsr123@naver.com>
2023-01-11 05:44:38 +0900

    feat: popup 스토리지 설정 기능 추가

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,11 +1,21 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 function Popup() {
   const [isToggled, setIsToggled] = useState(false);
 
+  useEffect(() => {
+    chrome.storage.local.get('enabled', (data) => {
+      setIsToggled(data.enabled);
+    });
+  }, []);
+
   const handleToggle = useCallback(() => {
-    setIsToggled((prevIsToggled) => !prevIsToggled);
+    setIsToggled((prevIsToggled) => {
+      const nextStatus = !prevIsToggled;
+      chrome.storage.local.set({ enabled: nextStatus });
+      return nextStatus;
+    });
   }, []);
 
   return (
