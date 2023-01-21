a1507fc9364cfb581e82551ddbb5818b049a1be2
zsr12383 <zsr123@naver.com>
2023-01-21 17:38:26 +0900

    feat: popup 토글 버튼 수정

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,5 +1,50 @@
 import React, { useCallback, useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+const CheckBoxWrapper = styled.div`
+  position: relative;
+`;
+const CheckBoxLabel = styled.label`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 42px;
+  height: 26px;
+  border-radius: 15px;
+  background: #bebebe;
+  cursor: pointer;
+  &::after {
+    content: '';
+    display: block;
+    border-radius: 50%;
+    width: 18px;
+    height: 18px;
+    margin: 3px;
+    background: #ffffff;
+    box-shadow: 1px 3px 3px 1px rgba(0, 0, 0, 0.2);
+    transition: 0.2s;
+  }
+`;
+const CheckBox = styled.input`
+  opacity: 0;
+  z-index: 1;
+  border-radius: 15px;
+  width: 42px;
+  height: 26px;
+  &:checked + ${CheckBoxLabel} {
+    background: #4fbe79;
+    &::after {
+      content: '';
+      display: block;
+      border-radius: 50%;
+      width: 18px;
+      height: 18px;
+      margin-left: 21px;
+      transition: 0.2s;
+    }
+  }
+`;
 
 function Popup() {
   const [isToggled, setIsToggled] = useState(false);
@@ -19,9 +64,10 @@ function Popup() {
   }, []);
 
   return (
-    <button type="button" onClick={handleToggle}>
-      {isToggled ? 'Turn off' : 'Turn on'}
-    </button>
+    <CheckBoxWrapper>
+      <CheckBox id="checkbox" type="checkbox" checked={isToggled} onChange={handleToggle} />
+      <CheckBoxLabel htmlFor="checkbox" />
+    </CheckBoxWrapper>
   );
 }
 
