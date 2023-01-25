c0ef4b8e31b2f8f93d7d575defff333bac521080
zsr12383 <zsr123@naver.com>
2023-01-25 18:19:29 +0900

    fix: 경로 가져오기로 변경
    import {} 문으로 슬라이더 가져올 시 material 라이브러리 전부 번들링 되는 문제가 있음

--- src/popup/TransparencyController.tsx
+++ src/popup/TransparencyController.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { Slider } from '@mui/material';
+import Slider from '@mui/material/Slider';
 
 const ControllerWrapper = styled.div`
   display: flex;
